d2ed4316137ee3f7a8d312186a9d38dd7d24d0e2
celine1208 <estel2721@gmail.com>
2025-03-13 12:48:14 +0900

    점주 아이디찾기

--- Deliway/admin/store_ok.php
+++ Deliway/admin/store_ok.php
@@ -1,12 +1,21 @@
 <?php
 session_start();
 
+include "../config/db.php";
+
 $store_idx = $_POST['store_idx'];
 $store_ing = $_POST['store_ing'];
+$user_id = $_POST['user_id'];
 
 $sql = "UPDATE store SET store_ing = '".$store_ing."' where store_idx='".$store_idx."'";
-$result = mysqli_query($con,$sql);
+mysqli_query($con,$sql);
 
+
+if ($store_ing == "승인"){
+    $sql1 = "UPDATE member set user_type = 2 where user_id='".$user_id."'";
+    mysqli_query($con,$sql1);
+}
+    
 ?>
 
 <script>
